24179ac8f5a093a5d6f8f04a4883fec00bed2de7
William19980425 <13233056901@163.com>
2023-07-17 16:59:22 +0800

    这是python第二章的练习内容

---
+++ apostrophe.py
@@ -0,0 +1,33 @@
+message = "One of Python's strength is its diverse community"
+print(message)
+print(2+3)
+print(3/2)
+print(10 ** 6)
+print(2 + 3*4)
+print((2+3)*4)
+
+print("====================================分割线====================================")
+
+print(0.1+0.1)
+print(0.1+0.1)
+print(0.1+0.2)
+
+print(3 * 0.2)
+print(3 * 0.1)
+print("====================================分割线====================================")
+
+#在书写很大的数时，可使用下划线将其中的位分组，使其更清晰易读
+universe_age = 1_4000_0000
+print(universe_age)
+
+#同时给多个变量赋值
+x,y,z = 0,1,2
+print(x)
+print(y)
+print(z)
+
+x= 5
+print(x)
+
+x=5.0
+print(x)
