cdb01a97c995c031738b40cd283a8416a56caea8
William19980425 <13233056901@163.com>
2023-07-17 21:36:43 +0800

    我删除了分散的文件

--- name.py
+++
@@ -1,9 +0,0 @@
-name = "ada lovelace"
-print(name.title())
-print(name.lower())
-print(name.upper())
-print("-----------------------")
-first_name = "ada"
-last_name = "lovelace"
-full_name = f"{first_name} {last_name}"
-print(full_name)
