156f97af2d1c7c3df9b89dd477e31a8b2faf7a11
William19980425 <13233056901@163.com>
2023-07-24 19:59:20 +0800

    chapter7

---
+++ chapter7/rollercoaster.py
@@ -0,0 +1,10 @@
+# height = int(input("How tall are youm,in inches?"))
+# print(height)
+# print("height's type is:" +str(type(height)))
+height = input("How tall are youm,in inches?")
+height = int(height)
+if height >= 48:
+    print("\nYou're tall enough to ride!")
+else:
+    print("\nYou're be able to ride when you're a little older")
+
